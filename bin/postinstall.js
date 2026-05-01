b5a35e6b7de25e04db4dc15119bffea4a7778e6c
Damien Rochat <dami.scoot3@gmail.com>
2026-05-01 10:27:16 +0200

    Test17

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -3,23 +3,18 @@ const { spawn } = require("child_process");
 const path = require("path");
 
 const isInteractive = Boolean(process.stdin.isTTY && process.stdout.isTTY);
-
-if (!isInteractive) {
-  process.exit(0);
-}
+if (!isInteractive) process.exit(0);
 
 const initCwd = process.env.INIT_CWD || process.cwd();
-
-// On cible directement le fichier local au lieu d'utiliser npx
-// __dirname est le dossier 'bin' de ton package
 const kickstartPath = path.join(__dirname, "kickstart.js");
 
 console.log(`🚀 Initialisation de SSO Bridge...`);
 
-const child = spawn("node", [kickstartPath], {
+// Sous Windows, on entoure le chemin de guillemets pour gérer les espaces
+const child = spawn("node", [`"${kickstartPath}"`], {
   stdio: "inherit",
-  cwd: initCwd, // On reste dans le dossier de l'utilisateur
-  shell: true,
+  cwd: initCwd,
+  shell: true, // Garde true pour Windows
 });
 
 child.on("close", (code) => {
