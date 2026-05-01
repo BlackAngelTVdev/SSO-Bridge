4d9aade42e44723d45085da0905e0ad82da5bc8e
Damien Rochat <dami.scoot3@gmail.com>
2026-05-01 10:55:01 +0200

    test22

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -6,3 +6,4 @@ console.log("\x1b[1m%s\x1b[0m", "  🔒 SSO-BRIDGE : Installation terminée !");
 console.log("\x1b[36m%s\x1b[0m", "--------------------------------------------------");
 console.log("\nPour configurer votre projet maintenant, lancez :");
 console.log("\x1b[33m\x1b[1m  npx sso-bridge\x1b[0m\n");
+process.exit(1);
